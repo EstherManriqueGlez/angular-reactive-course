f96b12da2808d6ded6a451955881f5df1e595b11
Esther Manrique <esthermanriqueepam@gmail.com>
2025-09-21 12:59:16 -0600

    Implement MessagesService and integrate error handling in CoursesStore; update components to display messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import { LoadingService } from './loading/loading.service';
-
-
+import { Component, OnInit } from "@angular/core";
+import { LoadingService } from "./loading/loading.service";
+import { MessagesService } from "./messages/messages.service";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [LoadingService],
-    standalone: false
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  standalone: false,
 })
-export class AppComponent implements  OnInit {
+export class AppComponent implements OnInit {
+  constructor() {}
 
-    constructor() {
+  ngOnInit() {}
 
-    }
-
-    ngOnInit() {
-
-
-    }
-
-  logout() {
-
-  }
-
+  logout() {}
 }
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,62 +1,63 @@
-import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+  ViewEncapsulation,
+} from "@angular/core";
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import {Course} from "../model/course";
-import {FormBuilder, Validators, FormGroup} from "@angular/forms";
-import moment from 'moment';
-import {catchError} from 'rxjs/operators';
-import {throwError} from 'rxjs';
-import { CoursesService } from '../services/courses.service';
-import { LoadingService } from '../loading/loading.service';
+import { Course } from "../model/course";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import moment from "moment";
+import { catchError } from "rxjs/operators";
+import { throwError } from "rxjs";
+import { CoursesService } from "../services/courses.service";
+import { LoadingService } from "../loading/loading.service";
+import { MessagesService } from "../messages/messages.service";
+import { CoursesStore } from "../services/courses.store";
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css'],
-    providers: [LoadingService],
-    standalone: false
+  selector: "course-dialog",
+  templateUrl: "./course-dialog.component.html",
+  styleUrls: ["./course-dialog.component.css"],
+  providers: [LoadingService, MessagesService],
+  standalone: false,
 })
 export class CourseDialogComponent implements AfterViewInit {
+  form: FormGroup;
 
-    form: FormGroup;
+  course: Course;
 
-    course:Course;
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private coursesStore: CoursesStore,
+    private messagesService: MessagesService
+  ) {
+    this.course = course;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService,
-        private loadingService: LoadingService
-    ) {
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required],
+    });
+  }
 
-        this.course = course;
+  ngAfterViewInit() {}
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+  save() {
+    const changes = this.form.value;
 
-    }
+    this.coursesStore.saveCourse(this.course.id, changes).subscribe();
 
-    ngAfterViewInit() {
+    this.dialogRef.close(changes);
+  }
 
-    }
-
-    save() {
-      const changes = this.form.value;
-
-      const saveCourse$ =this.coursesService.saveCourse(this.course.id, changes)
-
-      this.loadingService.showLoaderUntillCompleted(saveCourse$)
-      .subscribe(val => {
-        this.dialogRef.close(val);
-      });
-    }
-
-    close() {
-        this.dialogRef.close();
-    }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
